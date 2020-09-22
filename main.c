e154835027e8b8cd18ee791d890b83c0535874dd
amerritt1999 <65575127+amerritt1999@users.noreply.github.com>
2020-09-22 17:36:23 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -5,17 +5,36 @@
 #include <sys/time.h>
 #include <memory.h>
 
-static const long Num_To_Sort = 1000000000;
+//static const long Num_To_Sort = 1000000000;
+static const long Num_To_Sort = 100;
 
 // Sequential version of your sort
 // If you're implementing the PSRS algorithm, you may ignore this section
-void sort_s(int *arr) {
+void sort_s(int *arr, int n) {
+    // implements bubble sort on the array
+    for (int list_length = n; list_length >= 2; list_length--)
+    for (int i = 0; i < list_length-1; i++)
+    if (arr[i] > arr[i+1]) {
+        int tmp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = tmp;
+    }
 
 }
 
 // Parallel version of your sort
-void sort_p(int *arr) {
-
+void sort_p(int *arr, int n) {
+#pragma omp num_threads(100) {
+    // implements bubble sort on the array
+#pragma omp parallel for
+    for (int list_length = n; list_length >= 2; list_length--)
+#pragma omp parallel for
+        for (int i = 0; i < list_length-1; i++)
+            if (arr[i] > arr[i+1]) {
+                int tmp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = tmp;
+            }
 }
 
 int main() {
@@ -42,7 +61,7 @@ int main() {
 
     printf("Timing sequential...\n");
     gettimeofday(&start, NULL);
-    sort_s(arr_s);
+    sort_s(arr_s, Num_To_Sort);
     gettimeofday(&end, NULL);
     printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
 
@@ -50,7 +69,7 @@ int main() {
 
     printf("Timing parallel...\n");
     gettimeofday(&start, NULL);
-    sort_p(arr_p);
+    sort_p(arr_p, Num_To_Sort);
     gettimeofday(&end, NULL);
     printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
 
@@ -58,4 +77,3 @@ int main() {
 
     return 0;
 }
-
